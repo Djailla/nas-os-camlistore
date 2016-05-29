8f816882c7416d7d7e48cf780047b0110f90477a
Djailla <bastien.vallet@gmail.com>
2016-05-30 00:36:00 +0200

    Fix build script and startup script

--- com.djailla.camlistore/build.sh
+++ com.djailla.camlistore/build.sh
@@ -3,17 +3,22 @@
 mkdir -m 755 -p /opt/camlistore/
 install -m 755 /home/source/rc.local /etc
 
-# Install Golang
+# Install Git
 apt-get update
-apt-get install -y -q golang git
+apt-get install -y -q git
+
+# Install Golang
+cd /tmp
+wget https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.6.2.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
 
 # Checkout sources in /opt
 cd /opt/
 git clone https://camlistore.googlesource.com/camlistore
-cd /opt/camlistore/
-git checkout 0.9
 
 # Build the package
+cd /opt/camlistore/
 go run make.go
 
 exit 0
